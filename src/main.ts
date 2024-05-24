bfa8349191fe53d397e865712eb74552c69cd356
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-05-24 11:14:02 +0700

    fixing main file lisener file and packjson for prod

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT, '0.0.0.0');
+
+  // check env file
+  let prot = '3003';
+  let ip = 'localhost';
+  if (process.env.PORT) prot = process.env.PORT;
+  if (process.env.IP) ip = process.env.IP;
+
+  await app.listen(prot);
 }
 bootstrap();
